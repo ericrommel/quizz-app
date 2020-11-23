3a3a3de74e90878cd40c81b64629f7cf45d16e35
Eric Dantas <ericrommel@gmail.com>
2020-11-23 23:42:23 +0100

    Add test for Error Handlings. #6

--- backend/src/errors.py
+++ backend/src/errors.py
@@ -5,29 +5,29 @@ from flask import render_template, jsonify
 
 def bad_request(e):
     src.LOGGER.error(e)
-    return render_template("errors/400.html"), 400
+    return render_template("errors/400.html", title="Error 400"), 400
 
 
 def unauthorized(e):
     src.LOGGER.error(e)
-    return render_template("errors/400.html"), 401
+    return render_template("errors/401.html", title="Error 401"), 401
 
 
 def forbidden(e):
     src.LOGGER.error(e)
-    return render_template("errors/403.html"), 403
+    return render_template("errors/403.html", title="Error 403"), 403
 
 
 def page_not_found(e):
     src.LOGGER.error(e)
-    return render_template("errors/404.html"), 404
+    return render_template("errors/404.html", title="Error 404"), 404
 
 
 def method_not_allowed(e):
     src.LOGGER.error(e)
-    return render_template("errors/405.html"), 405
+    return render_template("errors/405.html", title="Error 405"), 405
 
 
 def internal_server_error(e):
     src.LOGGER.error(e)
-    return render_template("errors/500.html"), 500
+    return render_template("errors/500.html", title="Error 500"), 500
