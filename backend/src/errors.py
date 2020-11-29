7a60d750c6885175e048e83a17416be841efb9ee
Eric Dantas <ericrommel@gmail.com>
2020-11-29 12:39:24 +0100

    Send error messages to the template if needed

--- backend/src/errors.py
+++ backend/src/errors.py
@@ -5,29 +5,29 @@ from flask import render_template, jsonify
 
 def bad_request(e):
     src.LOGGER.error(e)
-    return render_template("errors/400.html", title="Error 400"), 400
+    return render_template("errors/400.html", error=e, title="Error 400"), 400
 
 
 def unauthorized(e):
     src.LOGGER.error(e)
-    return render_template("errors/401.html", title="Error 401"), 401
+    return render_template("errors/401.html", error=e, title="Error 401"), 401
 
 
 def forbidden(e):
     src.LOGGER.error(e)
-    return render_template("errors/403.html", title="Error 403"), 403
+    return render_template("errors/403.html", error=e, title="Error 403"), 403
 
 
 def page_not_found(e):
     src.LOGGER.error(e)
-    return render_template("errors/404.html", title="Error 404"), 404
+    return render_template("errors/404.html", error=e, title="Error 404"), 404
 
 
 def method_not_allowed(e):
     src.LOGGER.error(e)
-    return render_template("errors/405.html", title="Error 405"), 405
+    return render_template("errors/405.html", error=e, title="Error 405"), 405
 
 
 def internal_server_error(e):
     src.LOGGER.error(e)
-    return render_template("errors/500.html", title="Error 500"), 500
+    return render_template("errors/500.html", error=e, title="Error 500"), 500
